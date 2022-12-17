c98f98ee1261f20b2261718a944f8b97f9532466
nrenner <nrenner0211@gmail.com>
2022-12-17 17:38:12 -0500

    skills section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.scss';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
-    <NavBar/>
+    <div className='App'>
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
   );
 }
 
